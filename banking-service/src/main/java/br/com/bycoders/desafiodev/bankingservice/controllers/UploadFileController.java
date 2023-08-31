b424c00aa6a3ff235c3d2ae39efaf3f498246273
Salomão Batista <salomaum.batiata@gmail.com>
2023-08-31 02:03:25 -0300

    feat:
    Changes
    
    Adding swagger's configuration in the project whether dependency and configs
    Adding specification info to controllers

--- banking-service/src/main/java/br/com/bycoders/desafiodev/bankingservice/controllers/UploadFileController.java
+++ banking-service/src/main/java/br/com/bycoders/desafiodev/bankingservice/controllers/UploadFileController.java
@@ -4,6 +4,10 @@ import br.com.bycoders.desafiodev.bankingservice.domains.entity.Owner;
 import br.com.bycoders.desafiodev.bankingservice.domains.entity.Transactions;
 import br.com.bycoders.desafiodev.bankingservice.services.UploadFileService;
 import br.com.bycoders.desafiodev.bankingservice.validators.ValidMultipartFile;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -16,12 +20,18 @@ import java.util.Map;
 @RestController
 @RequestMapping("/upload-file")
 @Validated
+@Tag(name = "Upload File CNAB", description = "")
 public class UploadFileController {
 
     @Autowired
     private UploadFileService uploadFileService;
 
     @PostMapping
+    @Operation(summary = "This endpoint is resposible to receive the file thoughtout the client and persiste her into database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "File uploaded successfully"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")})
     public Map<Owner, List<Transactions>> uploadFile(@ValidMultipartFile @RequestBody MultipartFile file) throws IOException {
         return uploadFileService.uploadFile(file);
     }
