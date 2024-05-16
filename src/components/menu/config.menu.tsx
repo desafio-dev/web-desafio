48537805cd058cd7736ce55035d4fd0b5d639c20
Salomao123 <salomaum.batiata@gmail.com>
2024-05-16 16:39:32 -0300

    fix: corrigindo modal

--- src/components/menu/config.menu.tsx
+++ src/components/menu/config.menu.tsx
@@ -1,17 +1,18 @@
-import {MenuProps} from 'antd';
-import { MailOutlined } from '@ant-design/icons';
+import { MenuProps } from "antd";
+import { MailOutlined } from "@ant-design/icons";
 
-type MenuItem = Required<MenuProps>['items'][number];
+type MenuItem = Required<MenuProps>["items"][number];
+
 export const items: MenuItem[] = [
-    {
-        label: 'Backoffice - Processamento Remessa',
-        key: 'mail',
-        icon: <MailOutlined />,
-        disabled: true
-    },
-    {
-      label: 'Processar Remessa',
-      key: 'mail',
-      icon: <MailOutlined />,
-    },
-  ];
+  {
+    label: "Backoffice - Processamento Remessa",
+    key: "mail",
+    icon: <MailOutlined />,
+    disabled: true,
+  },
+  {
+    label: "Processar Remessa",
+    key: "mail",
+    icon: <MailOutlined />,
+  },
+];
